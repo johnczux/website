d59260590c9bd3d510cdbce195bd20ae1f18c226
johnczux <johnczux@gmail.com>
2020-07-04 02:26:13 -0400

    Finished Header along with added routing

--- PWebsite/src/app/app-routing.module.ts
+++ PWebsite/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { MainComponent } from './main/main.component';
+import { ExperimentalComponent } from './experimental/experimental.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'projects', component: ProjectsComponent},
+  {path : '', component: MainComponent},
+  {path : 'main', component: MainComponent},
+  {path : 'experimental', component: ExperimentalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {  }
