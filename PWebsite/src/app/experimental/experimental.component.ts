8ec603b9a2ae0beeecc30be482afa2ecb73269d2
johnczux <johnczux@gmail.com>
2020-09-04 21:50:27 -0400

    Random test items added to experimental, new icon added

--- PWebsite/src/app/experimental/experimental.component.ts
+++ PWebsite/src/app/experimental/experimental.component.ts
@@ -21,19 +21,18 @@ draw(event :MouseEvent)
   
    var box = document.getElementById("myCanvas");
    var rect = box.getBoundingClientRect();
-   console.log(rect.top, rect.bottom, rect.left, rect.right  );
-   console.log(y + " - " + x);
 
    if( (x > rect.left && x < rect.right) && ( y < rect.bottom && y > rect.top ) )
    {
 
     var relativeX = x - rect.left;
     var relativeY = y - rect.top;
-     console.log(relativeY + "-" + relativeX);
     const canvas = <HTMLCanvasElement> document.getElementById('myCanvas');
     const ctx = canvas.getContext('2d');
     ctx.beginPath();
-    ctx.arc(relativeX,relativeY,40,0, 2 * Math.PI);
+    ctx.arc(relativeX,relativeY,4,0, 2 * Math.PI);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
     ctx.stroke();
    }
 
